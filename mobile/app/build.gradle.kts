aa2b6043f1eaad94e20792b8770bc608cbfc8d57
xf8410 <1421506586@qq.com>
2026-08-20 00:52:15 +0800

    Create Android app shell for touch frontend

---
+++ mobile/app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android { namespace = "com.umaai.ramen"; compileSdk = 35
+    defaultConfig { applicationId = "com.umaai.ramen"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+}
+
+kotlin { jvmToolchain(17) }
+
+androidResources { noCompress += "json"; noCompress += "toml" }
